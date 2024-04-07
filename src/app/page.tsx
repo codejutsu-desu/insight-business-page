2f4bfd00940d640170a65cf23e4cc8084b1b7430
haqueAlquds <mrimmoy.shimanto@alquds.academy>
2024-04-08 00:23:19 +0600

    nam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-
 export default function page() {
-  return <div>Insight International School</div>;
+  return (
+    <div className="text-center mt-10 text-5xl">
+      Insight International School
+    </div>
+  );
 }
